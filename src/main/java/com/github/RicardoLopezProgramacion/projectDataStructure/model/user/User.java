be7a4dcc2902a3200c1ce850aa79ca1656ba8ee8
ricardoLopezProgramming <150400025+ricardoLopezProgramming@users.noreply.github.com>
2023-11-25 19:07:09 -0500

    Add attributes to the User class

--- src/main/java/com/github/RicardoLopezProgramacion/projectDataStructure/model/user/User.java
+++ src/main/java/com/github/RicardoLopezProgramacion/projectDataStructure/model/user/User.java
@@ -1,2 +1,25 @@
-package com.github.RicardoLopezProgramacion.projectDataStructure.model.user;public class User {
+package com.github.RicardoLopezProgramacion.projectDataStructure.model.user;
+
+import java.time.LocalDate;
+
+public class User {
+
+    // <data for user profile> {
+
+    // < data for login>
+
+    private String name;
+    private String lastName;
+    private String cellphone;
+    private String studentCode;
+
+    // </data for login>
+    private int age;
+    private String description;
+    private LocalDate dayOfBirth;
+    private String email;
+
+    // </data for user profile>
+
+
 }
